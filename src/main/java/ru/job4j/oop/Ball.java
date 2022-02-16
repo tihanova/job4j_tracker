c0ae579781bc7afe6196b7c71c7672c212442e8c
tihanova <a.tihanova@cdek.ru>
2022-02-16 15:37:00 +0300

    1.6. Взаимодействие объектов

---
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean a) {
+        if (a) {
+            System.out.println("Колобок съеден");
+        } else {
+            System.out.println("Колобок сбежал");
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/oop/BallStory.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class BallStory {
+    public static void main(String[] args) {
+        Ball ball = new Ball();
+        Fox fox = new Fox();
+        Wolf wolf = new Wolf();
+        Hare hare = new Hare();
+        hare.tryEat(ball);
+        wolf.tryEat(ball);
+        fox.tryEat(ball);
+    }
+}
