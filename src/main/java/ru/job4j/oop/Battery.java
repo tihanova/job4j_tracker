81c56be82c4bb81ef78221a66af3b9055a0bfa74
tihanova <a.tihanova@cdek.ru>
2022-02-16 17:23:38 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = this.load + another.load;
+        this.load = 0;
+    }
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public static void main(String[] args) {
+        Battery batteryfirst = new Battery(30);
+        Battery batterysecond = new Battery(50);
+        System.out.println("before");
+        System.out.println("batteryfirst load = " + batteryfirst.load);
+        System.out.println("batterysecond load = " + batterysecond.load);
+        batteryfirst.exchange(batterysecond);
+        System.out.println("after");
+        System.out.println("batteryfirst load = " + batteryfirst.load);
+        System.out.println("batterysecond load = " + batterysecond.load);
+
+    }
+}
