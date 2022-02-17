9be8cf86e3c56806392aaeff4988ae7cf4e0d268
tihanova <a.tihanova@cdek.ru>
2022-02-17 17:37:04 +0300

    1.6. Взаимодействие объектов [#166087 #265383] Исправления

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -1,8 +1,8 @@
 package ru.job4j.oop;
 
 public class Ball {
-    public void tryRun(boolean a) {
-        if (a) {
+    public void tryRun(boolean condition) {
+        if (condition) {
             System.out.println("Колобок съеден");
         } else {
             System.out.println("Колобок сбежал");
