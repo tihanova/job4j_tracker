bb7356ba87db69c255a07996f19794e00a1c8ec4
tihanova <a.tihanova@cdek.ru>
2022-03-20 13:55:32 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,13 @@
+package ru.job4j.pojo;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Тиханова Анастасия");
+        student.setGroup("943321");
+        student.setDateGo("01-12-2009");
+        System.out.println("Студент " + student.getFullName() + " обучается в вгруппе " + student.group + " с " + student.getDateGo());
+    }
+}
--- src/main/java/ru/job4j/pojo/Student.java
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -1,21 +1,43 @@
 package ru.job4j.pojo;
 
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+import java.util.Date;
+import java.text.ParseException;
 
 public class Student {
     public String fullName;
     public String group;
-    public LocalDateTime dateGo;
+    public Date dateGo;
 
-    public static void main(String[] args) {
-
+    public String getDateGo() {
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd-MM-yyyy");
+        return formatForDateNow.format(dateGo);
     }
 
-    public LocalDateTime getDateGo() {
-        return dateGo;
+    public void setDateGo(String dateGoting) {
+        try {
+            dateGo  = new SimpleDateFormat("dd-MM-yyyy").parse(dateGoting);
+
+        } catch (ParseException e) {
+            System.out.println("Дата должна быть введена в формате dd-MM-yyyy");
+        }
     }
 
-    public void setDateGo(LocalDateTime dateGo) {
-        this.dateGo = dateGo;
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
     }
 }
