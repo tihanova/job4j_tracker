823da6548071b0735321d8b9d5eca9164357d9e6
tihanova <a.tihanova@cdek.ru>
2022-05-11 22:10:32 +0300

    1. Singleton [#94606 #299446] - исправления

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -6,6 +6,8 @@ public class SingleTracker {
 
     private static SingleTracker instance = null;
 
+    private Tracker tracker = new Tracker();
+
     private SingleTracker() {
     }
 
@@ -15,8 +17,6 @@ public class SingleTracker {
         }
         return instance;
     }
-
-    private Tracker tracker = new Tracker();
 
     public Item add(Item item) {
         return tracker.add(item);
